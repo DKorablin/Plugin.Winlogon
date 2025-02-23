3f59874060de06df071fe5b4c66a53ae44aa49a0
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 02:31:35 +0300

    Unify plugins deployment * Fixed issue with SAL.Flatbed assebly + Added assembly inside repo (Will remove it in the future...)

--- Plugin.Winlogon/Properties/AssemblyInfo.cs
+++ Plugin.Winlogon/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("6b7a3e25-8cef-4549-85a0-915114875dce")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=121")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Winlogon")]
 [assembly: AssemblyDescription("Winlogon Notification Handler")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Winlogon")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2012-2024")]
 #endif
 
